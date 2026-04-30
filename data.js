520b6c5fd3093237a5653cee1a730700c78dfbfa
klimovila101-tech <klimovila101@gmail.com>
2026-05-01 03:24:28 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const boardData = {
+    dateStamp: "31.04.2026 #3",
+    legends: [
+        { title: "Главный болтун", value: "Кофе — 94.900 сооб." },
+        { title: "Болтун месяца", value: "Nobi967 — 9.800 сооб." },
+        { title: "Спонсоры квестов", value: "DEMO — 3000 руб." },
+        { title: "Звёздная реакция", value: "AKULKA — 63 шт." },
+        { title: "Новый админ", value: "РЕМ" }
+    ],
+    sheikhs: [
+        { title: "Главный шейх", value: "Nubasic — 9.001 руб." },
+        { title: "Монстры Boosty", value: "Nubasic — 3 месяца (9к)" },
+        { title: "Демоны Boosty", value: "Grimm — 1 месяц (500р)" },
+        { title: "Топ донат", value: "Olha Star — 774 руб." },
+        { title: "Донат месяца", value: "Parawziq — 300 руб." }
+    ],
+    hallOfFame: [
+        { title: "Первый админ чата", value: "GENBY" },
+        { title: "Первый подписчик boosty", value: "ЛЕОНИД БУБЛИК" },
+        { title: "Вечная легенда чата", value: "КОФЕ" },
+        { title: "Создатель Discord-сервера", value: "M1ZERE" },
+        { title: "Первый участник чата", value: "ХАЙЗЕНБЕРГ" },
+        { title: "Создатель Minecraft-сервера", value: "KIBR_RAMEN" }
+    ]
+};
