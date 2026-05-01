c80d8ad29b6d1a8541499003ed2696796820f117
MARKIS <klimovila101@gmail.com>
2026-05-01 13:59:16 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 const boardData = {
-    dateStamp: "31.04.2026 #3",
-    legends: [
-        { title: "Главный болтун", value: "Кофе — 94.900 сооб." },
-        { title: "Болтун месяца", value: "Nobi967 — 9.800 сооб." },
-        { title: "Звёздная реакция", value: "AKULKA — 63 шт." },
+    dateStamp: "01.05.2026 #1", // Эту дату ты видишь внизу сайта
+    legends: [ // ОСТАВЛЯЕМ ТАК (это ключ для кода)
+        { title: "Главный болтун", value: "Кофе — 94.900 сообщений" },
+        { title: "Болтун месяца", value: "Nobi967 — 9.800 сообщений" },
+        { title: "Звёздная реакция", value: "Акулка — 63 шт." },
         { title: "Новый админ", value: "РЕМ" }
     ],
-    sheikhs: [
-        { title: "Главный шейх", value: "Nubasic — 9.001 руб." },
+    sheikhs: [ // ОСТАВЛЯЕМ ТАК
+        { title: "Главный шейх", value: "Нубасик — 9001 руб." },
         { title: "Монстры Boosty", value: "Nubasic — 3 месяца (9к)" },
-        { title: "Демоны Boosty", value: "Grimm — 1 месяц (500р)" },
-        { title: "Топ донат", value: "Olha Star — 774 руб." },
+        { title: "Демоны Boosty", value: "Гримм — 1 месяц (500р)" },
+        { title: "Топ донатов", value: "Ольга Стар — 774 руб." },
         { title: "Донат месяца", value: "Parawziq — 300 руб." }
     ],
-    hallOfFame: [
-        { title: "Первый админ чата", value: "GENBY" },
+    hallOfFame: [ // ОСТАВЛЯЕМ ТАК
+        { title: "Первый администратор чата", value: "GENBY" },
         { title: "Первый подписчик boosty", value: "ЛЕОНИД БУБЛИК" },
         { title: "Вечная легенда чата", value: "КОФЕ" },
-        { title: "Создатель Discord-сервера", value: "M1ZERE" },
+        { title: "Создатель сервера Discord", value: "M1ZERE" },
         { title: "Первый участник чата", value: "ХАЙЗЕНБЕРГ" },
         { title: "Создатель Minecraft-сервера", value: "KIBR_RAMEN" }
     ]
